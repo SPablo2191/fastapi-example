82910ccbdf608fd06e1a1e0ac7fb90eb4a54fa70
SPablo2191 <pablosandoval2191@gmail.com>
2026-03-21 20:17:49 -0300

    fix: add cors allowed origins for everyone

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.controllers import heroes_router, sidekicks_router, villains_router
+from fastapi.middleware.cors import CORSMiddleware
 from app.repositories.database import Base,engine
 # creo mi instancia de FastAPI
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router=heroes_router, prefix=f"/api/v1")
 app.include_router(router=sidekicks_router, prefix=f"/api/v1")
